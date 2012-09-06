ba64d85e30a1171ccae73bb08f444e49bfd16fcd
jah.humes@gmail.com <jah.humes@gmail.com>
2012-09-06 09:26:21 -0600

    Updating Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
-
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
   def show
     @user = User.find(params[:id])
   end
-
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   def new
     @user = User.new
   end
@@ -17,8 +21,12 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
   def edit
-    @user = User.find(params[:id])
   end
   def update
     @user = User.find(params[:id])
@@ -29,5 +37,19 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
   end
 end
